b576165a4f8de6da00008690da5d4adffa5ba84b
Marina DELWAULLE <mdelwaul@e2r11p6.clusters.42paris.fr>
2022-12-01 23:44:31 +0100

    galere sur l'affichage du tree

--- RBtree.hpp
+++ RBtree.hpp
@@ -6,11 +6,16 @@
 /*   By: mdelwaul <mdelwaul@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/22 19:01:55 by mdelwaul          #+#    #+#             */
-/*   Updated: 2022/11/29 19:09:10 by mdelwaul         ###   ########.fr       */
+/*   Updated: 2022/12/01 22:43:34 by mdelwaul         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <memory>
+#include <iomanip>
+#include <vector>
+#include <map>
+#include <iterator>
+#include <iostream>
 
 namespace ft
 {
@@ -18,10 +23,32 @@ namespace ft
 	class RBnode{
 		public:
 			T				content;
-			struct RBnode*	left;
-			struct RBnode*	right;
-			struct RBnode*	parent;
+			RBnode*			left;
+			RBnode*			right;
+			RBnode*			parent;
 			bool			colour;
+
+			size_t			getDepth(size_t depth = 0)
+			{
+				size_t add;
+				
+				if (!this)
+					return (depth);
+				depth++;
+				if (right)
+				{
+					add = right->getDepth(depth);
+					if (add > depth)
+						depth = add;
+				}
+				if (left)
+				{
+					add = left->getDepth(depth);
+					if (add > depth)
+						depth = add;
+				}
+				return (depth);
+			}
 	};
 
 	template <class T, class Allocator = std::allocator<RBnode<T>> >
@@ -49,13 +76,13 @@ namespace ft
 			}
 		
 		public:
-			RBnode()
+			RBtree()
 			{
-				_trunck = NULL;
+				_trunk = NULL;
 				_leftest = NULL;
 				_rightest = NULL;
 			}
-			RBnode(T val)
+			RBtree(T val)
 			{
 				_trunk = createNode(val, NULL, Allocator);
 				_leftest = _trunk;
@@ -69,7 +96,7 @@ namespace ft
 				RBnode *next = _trunk;
 				RBnode *parent = NULL;
 
-				if (!_trunck)
+				if (!_trunk)
 				{
 					_trunk = createNode(val, NULL, Allocator);
 					_leftest = _trunk;
@@ -91,6 +118,52 @@ namespace ft
 			}
 			void			deleteNode(T* ptr)
 			{
+				
+			}
+
+			void fillTab(std::map<int, vector<T*> > &tab, RBnode* node, int level, int maxlevel)
+			{
+				if (!node)
+				{
+					tab[level].push_back(NULL);
+					level++;
+					if (level < maxlevel)
+					{
+						fillTab(tab, NULL, level, maxlevel);
+						fillTab(tab, NULL, level, maxlevel);
+					}
+				}
+				else
+				{
+					tab[level].push_back(node->content);
+					level++;
+					if (level < maxlevel)
+					{
+						fillTab(tab, node->right, level, maxlevel);
+						fillTab(tab, node->left, level, maxlevel);
+					}
+				}
+			}
+			void	print(void)
+			{
+				if (!_trunk)
+				{
+					std::cout << "Cet arbre est vide" << std::endl;
+					return ;
+				}
+				std::map<int, std::vector<T*> > tab; //int pour la pronfondeur, mettre NULL si le node n existe pas
+				int depth = _trunk->getDepth();
+				fillTab(tab, _trunk, 0, depth);
+				for(std::map<int, std::vector<T*> >::iterator it = tab.begin(); it != tab.end(); it++)
+				{
+					for (std::vector<T*>::iterator ite = (*it).second.begin(); ite != (*it).second.end(); ite++)
+					{
+						for (int i = 0; i < depth * 5 / (*it).first; i++)
+							std::cout << " ";
+						std::cout << std::setw(10) << std::setiosflags(std::ios::right) *it;
+					}
+					std::cout << std::endl << std::endl;
+				}
 				
 			}
 	};
